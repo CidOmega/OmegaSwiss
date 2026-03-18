ae65a60201e3f0dfe49f9f206f9a289654da334a
Cid <cid@dic>
2026-03-18 23:27:44 +0100

    Rondas necesarias

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ export function setupTournament() {
     let playerTable = $('table#player-table');
     let playerTableBody = playerTable.find('tbody');
 
+    let roundDisplay = $('#roundDisplay');
+
     $('button#new-player').on('click', () => {
         let playerName = playerNameInput.val()?.toString() ?? "";
         if (playerName === "") {
@@ -16,11 +18,18 @@ export function setupTournament() {
         let newPlayer: Player = {id: crypto.randomUUID(), name: playerName};
         players.push(newPlayer)
         playerTableBody.append('<tr><td>' + newPlayer.name + '</td></tr>');
+        updateRoundDisplay()
     });
 
     $('button#export-players').on('click', () => {
         console.log(players);
     });
+    
+    function updateRoundDisplay() {
+        let rounds = players.length == 0 ? 0 : Math.ceil(Math.log2(players.length));
+        roundDisplay.html('Rondas necesarias: ' + rounds);
+    }
+    updateRoundDisplay()
 }
 
 setupTournament();
