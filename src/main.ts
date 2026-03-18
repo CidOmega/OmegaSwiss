e77bdc48d97b559439843be2a6779e78a10dc2a7
Cid <cid@dic>
2026-03-18 23:18:04 +0100

    Player list

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import {Player} from "./Models/Player.ts";
 
 export function setupTournament() {
     let players: Player[] = [];
+    let playerNameInput = $('input#player-name-input');
+    let playerTable = $('table#player-table');
+    let playerTableBody = playerTable.find('tbody');
 
     $('button#new-player').on('click', () => {
-        players.push({id: crypto.randomUUID(), name: crypto.randomUUID()})
+        let playerName = playerNameInput.val()?.toString() ?? "";
+        if (playerName === "") {
+            return;
+        }
+        playerNameInput.val("")
+
+        let newPlayer: Player = {id: crypto.randomUUID(), name: playerName};
+        players.push(newPlayer)
+        playerTableBody.append('<tr><td>' + newPlayer.name + '</td></tr>');
     });
 
-    $('button#export-player').on('click', () => {
+    $('button#export-players').on('click', () => {
         console.log(players);
     });
 }
