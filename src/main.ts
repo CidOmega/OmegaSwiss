3ac6e286fe995419afe13b0d69b7b5b9cb15e907
Cid <cid@dic>
2026-03-22 21:10:43 +0100

    setupPlayersController

--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,7 @@
-import {Player} from "./Models/Player.ts";
+import {setupPlayersController} from "./Controllers/PlayersController.ts";
 
-export function setupTournament() {
-    let players: { [id: string]: Player } = {};
-    let playerNameInput = $('input#player-name-input');
-    let playerTable = $('table#player-table');
-    let playerTableBody = playerTable.find('tbody');
-
-    // Grab the template and destroy it
-    let playerRowTemplate = playerTableBody.html();
-    playerTableBody.html('')
-
-    let requiredRoundDisplay = $('#requiredRoundDisplay');
-
-    $('button#new-player').on('click', () => {
-        let playerName = playerNameInput.val()?.toString() ?? "";
-        if (playerName === "") {
-            return;
-        }
-        playerNameInput.val("")
-
-        let newPlayer: Player = {id: crypto.randomUUID(), name: playerName};
-        players[newPlayer.id] = newPlayer;
-        updatedPlayers()
-        playerNameInput.trigger('focus');
-    });
-
-    $('button#export-players').on('click', () => {
-        console.log(players);
-    });
-
-    function renderPlayers() {
-        playerTableBody.html('')
-        for (let key in players) {
-            let player = players[key];
-            let newRowHtml = playerRowTemplate;
-            newRowHtml = newRowHtml.replace('${playerId}', player.id);
-            newRowHtml = newRowHtml.replace('${playerName}', player.name);
-            playerTableBody.append(newRowHtml);
-        }
-
-        $('button.btn-delete-player').on('click', (e) => {
-            let playerId = $(e.target).attr('data-related') ?? "";
-            delete players[playerId];
-            updatedPlayers();
-        })
-    }
-
-    function updateRequiredRoundDisplay() {
-        let playersLength = Object.keys(players).length;
-        let rounds = playersLength == 0 ? 0 : Math.ceil(Math.log2(playersLength));
-        requiredRoundDisplay.html('Rondas necesarias: ' + rounds);
-    }
-
-    function updatedPlayers() {
-        renderPlayers();
-        updateRequiredRoundDisplay();
-    }
-
-    updatedPlayers();
+export function setupApp() {
+    setupPlayersController()
 }
 
-setupTournament();
+setupApp();
