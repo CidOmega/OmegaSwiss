89a61c2ac5e1e8ec1b15c28035c3a42f675f1499
Cid <cid@dic>
2026-03-23 22:44:51 +0100

    Tournament header

--- src/Controllers/TournamentController.ts
+++ src/Controllers/TournamentController.ts
@@ -1,26 +1,34 @@
 export function setupTournament() {
     let drawIsDraw = false;
-    
-    let swapDrawContainer = $('#swapDrawContainer');
-    let setDrawButton = swapDrawContainer.find('.btn-draw')
-    let setDoubleKoButton = swapDrawContainer.find('.btn-double-ko')
-    
+
+    let startTournament = $('#startTournament');
     let roundCountDisplay = $('#roundCountDisplay');
     roundCountDisplay.html('Ronda 1/9');
 
-    setDrawButton.on('click', function(){
+    let swapDrawContainer = $('#swapDrawContainer');
+    let setDrawButton = swapDrawContainer.find('.btn-draw')
+    let setDoubleKoButton = swapDrawContainer.find('.btn-double-ko')
+
+    startTournament.show();
+    roundCountDisplay.hide();
+    startTournament.on('click', () => {
+        startTournament.hide();
+        roundCountDisplay.show();
+    });
+
+    setDrawButton.on('click', function () {
         drawIsDraw = true;
         renderSwapDraw()
-    })
-    setDoubleKoButton.on('click', function(){
+    });
+    setDoubleKoButton.on('click', function () {
         drawIsDraw = false;
         renderSwapDraw();
-    })
-    
+    });
+
     function renderSwapDraw() {
         setDrawButton.toggle(!drawIsDraw);
         setDoubleKoButton.toggle(drawIsDraw);
     }
-    
+
     renderSwapDraw();
 }
