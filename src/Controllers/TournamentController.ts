4500b8a0d9b7bb1f53a372d7c140b34c1fccf2ad
Cid <cid@dic>
2026-03-23 23:31:27 +0100

    Fill Tournament table prototype

--- src/Controllers/TournamentController.ts
+++ src/Controllers/TournamentController.ts
@@ -1,3 +1,5 @@
+import {Player} from "../Models/Player.ts";
+
 export function setupTournament() {
     let drawIsDraw = false;
 
@@ -9,11 +11,20 @@ export function setupTournament() {
     let setDrawButton = swapDrawContainer.find('.btn-draw')
     let setDoubleKoButton = swapDrawContainer.find('.btn-double-ko')
 
+    let mainTable = $('#mainTable');
+    let mainTableBody = mainTable.find('tbody');
+
     startTournament.show();
     roundCountDisplay.hide();
     startTournament.on('click', () => {
         startTournament.hide();
         roundCountDisplay.show();
+
+        mainTableBody.html('');
+        mainTableBody.append(getMatchRowHtml({id: '1', name: 'Andy'}, {id: '2', name: 'Lucas'}, 'guid1', 1));
+        mainTableBody.append(getMatchRowHtml({id: '3', name: 'Cid'}, {id: 'X', name: 'Bye'}, 'guid2', 2));
+
+        renderSwapDraw()
     });
 
     setDrawButton.on('click', function () {
@@ -28,6 +39,31 @@ export function setupTournament() {
     function renderSwapDraw() {
         setDrawButton.toggle(!drawIsDraw);
         setDoubleKoButton.toggle(drawIsDraw);
+
+        mainTable.find('.btn-draw').toggle(drawIsDraw);
+        mainTable.find('.btn-double-ko').toggle(!drawIsDraw);
+    }
+
+    function getMatchRowHtml(player1: Player, player2: Player, matchId: string, tableNumber: number) {
+        return `
+    <tr class="match-row">
+    <th scope="row" class="text-center">${tableNumber}</th>
+    <td data-related="${player1.id}" class="player-cell">
+        <button type="button" data-related="${player1.id}" class="btn-retreat btn btn-secondary">Retirada</button>
+        ${player1.name}
+        <button type="button" data-related="${player1.id}" class="btn-win btn btn-success float-end">Victoria</button>
+    </td>
+    <td>
+        <button type="button" data-related="${matchId}" class="btn-draw btn btn-warning col-12">Empate</button>
+        <button type="button" data-related="${matchId}" class="btn-double-ko btn btn-danger col-12 text-nowrap">Doble KO</button>
+    </td>
+    <td data-related="${player2.id}" class="player-cell">
+        <button type="button" data-related="${player2.id}" class="btn-retreat btn btn-secondary">Retirada</button>
+        ${player2.name}
+        <button type="button" data-related="${player2.id}" class="btn-win btn btn-success float-end">Victoria</button>
+    </td>
+    </tr>
+`
     }
 
     renderSwapDraw();
