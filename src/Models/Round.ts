42e8350ba619b1d77c1e6abefb411847fc619820
Cid <cid@dic>
2026-03-24 21:44:22 +0100

    Round to class

--- src/Models/Round.ts
+++ src/Models/Round.ts
@@ -1,7 +1,22 @@
 import {Match} from "./Match.ts";
 import {Player} from "./Player.ts";
 
-export type Round = {
+export class Round {
     matches: Match[];
     retreats: Player[];
+
+    constructor(matches: Match[]) {
+        this.matches = matches;
+        this.retreats = [];
+    }
+
+    getAllPlayers(): Player[] {
+        let response: Player[] = [];
+        for (let match of this.matches) {
+            for (let result of match.results) {
+                response.push(result.player);
+            }
+        }
+        return response;
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,29 +6,26 @@ import {MatchResultEnum} from "./Models/MatchResultEnum.ts";
 export function setupApp() {
     setupPlayersController();
 
-    let testRound: Round = {
-        matches: [
-            {
-                results: [
-                    {result: MatchResultEnum.None, player:{id: '1', name: 'Andy'}},
-                    {result: MatchResultEnum.None, player:{id: '2', name: 'Lucas'}},
-                ],
-            },
-            {
-                results: [
-                    {result: MatchResultEnum.None, player:{id: '3', name: 'Cid'}},
-                    {result: MatchResultEnum.None, player:{id: '4', name: 'Omega'}},
-                ],
-            },
-            {
-                results: [
-                    {result: MatchResultEnum.None, player:{id: '5', name: 'King'}},
-                    {result: MatchResultEnum.None, player:{id: 'X', name: 'Bye'}},
-                ],
-            },
-        ],
-        retreats: [],
-    };
+    let testRound = new Round([
+        {
+            results: [
+                {result: MatchResultEnum.None, player: {id: '1', name: 'Andy'}},
+                {result: MatchResultEnum.None, player: {id: '2', name: 'Lucas'}},
+            ],
+        },
+        {
+            results: [
+                {result: MatchResultEnum.None, player: {id: '3', name: 'Cid'}},
+                {result: MatchResultEnum.None, player: {id: '4', name: 'Omega'}},
+            ],
+        },
+        {
+            results: [
+                {result: MatchResultEnum.None, player: {id: '5', name: 'King'}},
+                {result: MatchResultEnum.None, player: {id: 'X', name: 'Bye'}},
+            ],
+        },
+    ]);
     setupRound(testRound);
 }
 
