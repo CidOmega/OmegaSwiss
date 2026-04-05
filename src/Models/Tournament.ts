f4df934758e541529b63d9f997638ed1b0e1802c
Cid <cid@dic>
2026-04-05 21:08:01 +0200

    Remove TODO

--- src/Models/Tournament.ts
+++ src/Models/Tournament.ts
@@ -96,11 +96,6 @@ export class Tournament {
             } else {
                 matches.push(getNewMatch(playerPointer.player, rival.player))
 
-                // TODO estos dos jugadores han dejado de estar disponibles,
-                //  asi que "availableRivals" de los jugadores restantes hay que actualizarlo
-                //  para que si a alguien le quedan 2 el 2º lo pesque.
-                //  ¿Que pasa si alguien tiene 2 disponibles y se emparejan entre ellos?
-
                 Tools.deleteFromArray(playersWithAvailableRivals, rival);
 
                 // playerPointer and rival is not available for the rest of the pairing
