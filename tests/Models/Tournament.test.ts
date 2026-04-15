d33b3b81a5d8d79b9356961719bfd20c563b020e
CidOmega <edgar-cid@hotmail.com>
2026-04-16 00:02:50 +0200

    Feature/time travel/tournament contains rounds (#35)
    * Tournament.ts refactor
    
    * Tournament.test.ts
    
    ---------
    
    Co-authored-by: Cid <cid@dic>

--- tests/Models/Tournament.test.ts
+++ tests/Models/Tournament.test.ts
@@ -3,6 +3,7 @@ import {Tournament} from "../../src/Models/Tournament";
 import {PlayerHistory} from "../../src/Models/PlayerHistory";
 import {Tools} from "../../src/Tools";
 import {MatchResultEnum} from "../../src/Models/MatchResultEnum";
+import {Round} from "../../src/Models/Round";
 
 function newPlayer(data: string | null = null): Player {
     return {
@@ -29,9 +30,12 @@ describe('Tournament.getActivePlayers', () => {
         // Arrange
         let players: Player[] = [newPlayer(), newPlayer(), newPlayer()];
         let tournament: Tournament = new Tournament(players);
-        tournament.retreats.push(newPlayer());
-        tournament.retreats.push(newPlayer());
-        tournament.retreats.push(newPlayer());
+        let round = new Round([]);
+
+        round.retreats.push(newPlayer());
+        round.retreats.push(newPlayer());
+        round.retreats.push(newPlayer());
+        tournament.rounds.push(round);
 
         // Act
         let activePlayers: PlayerHistory[] = tournament.getActivePlayers();
@@ -44,9 +48,12 @@ describe('Tournament.getActivePlayers', () => {
         // Arrange
         let players: Player[] = [newPlayer(), newPlayer(), newPlayer()];
         let tournament: Tournament = new Tournament(players);
-        tournament.retreats.push(players[0]);
-        tournament.retreats.push(players[1]);
-        tournament.retreats.push(players[2]);
+        let round = new Round([]);
+
+        round.retreats.push(players[0]);
+        round.retreats.push(players[1]);
+        round.retreats.push(players[2]);
+        tournament.rounds.push(round);
 
         // Act
         let activePlayers: PlayerHistory[] = tournament.getActivePlayers();
@@ -59,8 +66,11 @@ describe('Tournament.getActivePlayers', () => {
         // Arrange
         let players: Player[] = [newPlayer(), newPlayer(), newPlayer()];
         let tournament: Tournament = new Tournament(players);
-        tournament.retreats.push(players[0]);
-        tournament.retreats.push(players[2]);
+        let round = new Round([]);
+
+        round.retreats.push(players[0]);
+        round.retreats.push(players[2]);
+        tournament.rounds.push(round);
 
         // Act
         let activePlayers: PlayerHistory[] = tournament.getActivePlayers();
@@ -72,48 +82,100 @@ describe('Tournament.getActivePlayers', () => {
 });
 
 describe('Tournament.getRanking', () => {
+    // Arrange
+    let players: Player[] = [Tools.byeId, 1, 2, 3, 4, 5, 6, 7].map(n => newPlayer(n.toString()));
+    // 0 is bye
+    let tournament: Tournament = new Tournament(players.filter(p => p.id !== players[0].id));
+
+    let round1 = new Round([]);
+    round1.matches = [
+        {
+            results: [
+                {player: players[1], result: MatchResultEnum.Win},
+                {player: players[7], result: MatchResultEnum.Lose},
+            ],
+        },
+        {
+            results: [
+                {player: players[2], result: MatchResultEnum.Win},
+                {player: players[5], result: MatchResultEnum.Lose},
+            ],
+        },
+        {
+            results: [
+                {player: players[3], result: MatchResultEnum.Win},
+                {player: players[6], result: MatchResultEnum.Lose},
+            ],
+        },
+        {
+            results: [
+                {player: players[4], result: MatchResultEnum.Win},
+                {player: players[0], result: MatchResultEnum.Lose},
+            ],
+        },
+    ];
+
+    let round2 = new Round([]);
+    round2.matches = [
+        {
+            results: [
+                {player: players[1], result: MatchResultEnum.Win},
+                {player: players[4], result: MatchResultEnum.Lose},
+            ],
+        },
+        {
+            results: [
+                {player: players[2], result: MatchResultEnum.Win},
+                {player: players[3], result: MatchResultEnum.Lose},
+            ],
+        },
+        {
+            results: [
+                {player: players[5], result: MatchResultEnum.Win},
+                {player: players[6], result: MatchResultEnum.Lose},
+            ],
+        },
+        {
+            results: [
+                {player: players[7], result: MatchResultEnum.Win},
+                {player: players[0], result: MatchResultEnum.Lose},
+            ],
+        },
+    ];
+
+    let round3 = new Round([]);
+    round3.matches = [
+        {
+            results: [
+                {player: players[1], result: MatchResultEnum.Win},
+                {player: players[2], result: MatchResultEnum.Lose},
+            ],
+        },
+        {
+            results: [
+                {player: players[3], result: MatchResultEnum.Win},
+                {player: players[4], result: MatchResultEnum.Lose},
+            ],
+        },
+        {
+            results: [
+                {player: players[5], result: MatchResultEnum.Win},
+                {player: players[7], result: MatchResultEnum.Lose},
+            ],
+        },
+        {
+            results: [
+                {player: players[6], result: MatchResultEnum.Win},
+                {player: players[0], result: MatchResultEnum.Lose},
+            ],
+        },
+    ];
+
+    tournament.rounds.push(round1);
+    tournament.rounds.push(round2);
+    tournament.rounds.push(round3);
+
     test('compareTiebreaker always returns non zero', () => {
-        // Arrange
-        let players: Player[] = [0, 1, 2, 3, 4, 5, 6, 7].map(n => newPlayer(n.toString()));
-        let tournament: Tournament = new Tournament(players);
-
-        // En el orden del ranking porque lo estoy copiando.
-        tournament.allPlayerHistories[1].matchResults = [
-            {player: players[7], result: MatchResultEnum.Win},
-            {player: players[4], result: MatchResultEnum.Win},
-            {player: players[2], result: MatchResultEnum.Win},
-        ];
-        tournament.allPlayerHistories[2].matchResults = [
-            {player: players[5], result: MatchResultEnum.Win},
-            {player: players[3], result: MatchResultEnum.Win},
-            {player: players[1], result: MatchResultEnum.Lose},
-        ];
-        tournament.allPlayerHistories[3].matchResults = [
-            {player: players[6], result: MatchResultEnum.Win},
-            {player: players[2], result: MatchResultEnum.Lose},
-            {player: players[4], result: MatchResultEnum.Win},
-        ];
-        tournament.allPlayerHistories[5].matchResults = [
-            {player: players[2], result: MatchResultEnum.Lose},
-            {player: players[6], result: MatchResultEnum.Win},
-            {player: players[7], result: MatchResultEnum.Win},
-        ];
-        tournament.allPlayerHistories[4].matchResults = [
-            {player: players[0], result: MatchResultEnum.Win},
-            {player: players[1], result: MatchResultEnum.Lose},
-            {player: players[3], result: MatchResultEnum.Lose},
-        ];
-        tournament.allPlayerHistories[7].matchResults = [
-            {player: players[1], result: MatchResultEnum.Lose},
-            {player: players[0], result: MatchResultEnum.Win},
-            {player: players[5], result: MatchResultEnum.Lose},
-        ];
-        tournament.allPlayerHistories[6].matchResults = [
-            {player: players[3], result: MatchResultEnum.Lose},
-            {player: players[5], result: MatchResultEnum.Lose},
-            {player: players[0], result: MatchResultEnum.Win},
-        ];
-
         // Act
         let ranking = tournament.getRanking();
 
@@ -125,5 +187,41 @@ describe('Tournament.getRanking', () => {
                 throw new Error(`${ranking[i - 1].player.name} vs ${ranking[i].player.name} returned zero.`);
             }
         }
+    });
+
+    it.each([
+        [players[1].id, '3-0-0'],
+        [players[2].id, '2-1-0'],
+        [players[3].id, '2-1-0'],
+        [players[4].id, '1-2-0'],
+        [players[5].id, '2-1-0'],
+        [players[6].id, '1-2-0'],
+        [players[7].id, '1-2-0'],
+    ])('kda result', (playerId: string, expectedKda: string) => {
+        // Act
+        let ranking = tournament.getRanking();
+
+        // Assert
+        let tiebreaker = ranking.find(t => t.player.id === playerId);
+        expect(tiebreaker).not.toBe(undefined);
+        expect(tiebreaker!.kda).toStrictEqual(expectedKda);
+    });
+
+    it.each([
+        [players[1].id, 343],
+        [players[2].id, 27],
+        [players[3].id, 125],
+        [players[4].id, 1],
+        [players[5].id, 216],
+        [players[6].id, 64],
+        [players[7].id, 8],
+    ])('binary result', (playerId: string, expectedBinary: number) => {
+        // Act
+        let ranking = tournament.getRanking();
+
+        // Assert
+        let tiebreaker = ranking.find(t => t.player.id === playerId);
+        expect(tiebreaker).not.toBe(undefined);
+        expect(tiebreaker!.binary).toStrictEqual(expectedBinary);
     });
 });
