2412ab25c6b89ef6ab75e26c43337acecb59ff41
Cid <cid@dic>
2026-04-16 18:11:59 +0200

    remove saveRound

--- src/Storage/TournamentStorage.ts
+++ src/Storage/TournamentStorage.ts
@@ -1,5 +1,4 @@
 import {Tournament} from "../Models/Tournament.ts";
-import {Round} from "../Models/Round.ts";
 
 let tournamentCache: Tournament | null = null;
 const keyTournament = 'tournament';
@@ -17,17 +16,9 @@ export const TournamentStorage = {
         }
         return tournamentCache;
     },
-    getRound(): Round {
-        return this.getTournament().activeRound;
-    },
     saveTournament(tournament: Tournament | null = null) {
         tournamentCache = tournament ?? tournamentCache;
         window.localStorage.setItem(keyTournament, JSON.stringify(tournamentCache));
-    },
-    saveRound(round: Round | null = null) {
-        let tournament = this.getTournament();
-        tournament.activeRound = round ?? tournament.activeRound;
-        this.saveTournament(tournament);
     },
     deleteAll() {
         window.localStorage.removeItem(keyTournament);
